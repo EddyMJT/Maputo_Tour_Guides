a49c392fa511f02e1f3f56f74df8e7485ce094e3
Edilson Manjate <eddilsonmanjate@gmail.com>
2025-03-15 22:43:56 +0200

    Add blog posts(Entries)

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
-from .models import Topic
+from .models import Topic, Entry
 from django.contrib.auth.decorators import login_required
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def blog(request):
     """Main page of blog. Shows all blog posts"""
@@ -24,7 +24,15 @@ def topic(request, topic_id, topic_title):
 
 
 def topic_entry(request, topic_id, entry_id, entry_title):
-    return render(request, 'blog/topic_entry.html', {})
+    """Renders the text and image for individual post"""
+    topics = Topic.objects.all()
+    entries = Entry.objects.all()
+
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    context = {'topic': topic, 'entry': entry, 'topics': topics, 'entries': entries}
+
+    return render(request, 'blog/topic_entry.html', context)
 
 
 @login_required()
@@ -50,8 +58,25 @@ def edit_topic(request, topic_id):
     return render(request, "blog/edit_topic.html", {})
 
 
+@login_required()
 def new_topic_entry(request, topic_id):
-    return render(request, "blog/new_topic_entry.html", {})
+
+    topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user and not request.user.is_superuser:
+        raise Http404
+
+    if request.method != "POST":
+        form = EntryForm()
+    else:
+        form = EntryForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_topic_entry = form.save(commit=False)
+            new_topic_entry.topic = topic
+            new_topic_entry.save()
+        return HttpResponseRedirect(reverse("blog:topic", args=[topic_id, topic.slug_title]))
+    context = {"form": form, "topic": topic}
+
+    return render(request, "blog/new_topic_entry.html", context)
 
 
 def edit_topic_entry(request, entry_id):
