e11ced31a8d627a2cc8fdc5063ff0790c9ada6fb
Edilson Manjate <eddilsonmanjate@gmail.com>
2025-03-19 21:33:38 +0200

    tweaked app for heroku

--- maputo_tour_guides/settings.py
+++ maputo_tour_guides/settings.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 import os.path
+import django_heroku
+import dj_database_url
+from decouple import config
 
-from decouple import config, Csv
-
-
-from django.conf.global_settings import EMAIL_HOST_PASSWORD
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -17,13 +16,9 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default='dsadadasdadsadasdsa')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-
 ALLOWED_HOSTS = []
 
-
-
 # SMTP email configuration for Gmail SMTP
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -31,8 +26,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-# End of email configuration
-
 
 
 # Application definition
@@ -63,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'maputo_tour_guides.urls'
@@ -133,8 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mtg', 'static'),
+    os.path.join(BASE_DIR, 'tours', 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = '/users/login/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -154,3 +149,4 @@ BOOTSTRAP3 = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_heroku.settings(locals())
